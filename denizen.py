7a77f25f3aca34a0f071de0903a1fd38eb982da7
TheCambridgeRed <dan.elbro@gmail.com>
2019-02-07 17:17:07 +0000

    Add files via upload

--- denizen.py
+++ denizen.py
@@ -7,35 +7,52 @@ class Denizen:
     def draw_to(self, dest):
         dest.blit(self.sprite, (self.x, self.y))
 
-
-class Player(Denizen):
-    def __init__(self, x, y, vel_x, vel_y,
-                 accel_x, accel_y, sprite, rect):
-        self.x = x
-        self.y = y
-        self.vel_x = vel_x
-        self.vel_y = vel_y
-        self.accel_x = accel_x
-        self.accel_y = accel_y
-        self.sprite = sprite
-        self.rect = rect
-
-    def calculate_position(self):
+    def calculate_position(self, x_direction, terminal):
+        self.accelerate_x(x_direction)
+        self.accelerate_y(terminal)
         self.x += self.vel_x
         self.y += self.vel_y
 
-    def accelerate_x(self, direction, top_speed):
-        top_speed *= direction
-        accel_factor = 0.4
+    def accelerate_x(self, direction):
+        top_speed = self.run_speed * direction
+        accel_factor = 0.1
         self.vel_x = accel_factor * top_speed + (1 - accel_factor) * self.vel_x
         if abs(self.vel_x) < 0.1:
             self.vel_x = 0
 
     def accelerate_y(self, top_speed):
-        accel_factor = 0.1
+        accel_factor = 0.07
         self.vel_y = accel_factor * top_speed + (1 - accel_factor) * self.vel_y
         if abs(self.vel_y) < 0.1:
             self.vel_y = 0
 
-    def jump(self):
-        self.vel_y = -30
+
+class Player(Denizen):
+    def __init__(self, x, y, vel_x, vel_y, run_speed, terminal,
+                 accel_x, accel_y, jumping, sprite, rect):
+        self.x = x
+        self.y = y
+        self.vel_x = vel_x
+        self.vel_y = vel_y
+        self.run_speed = run_speed
+        self.terminal = terminal
+        self.accel_x = accel_x
+        self.accel_y = accel_y
+        self.jumping = jumping
+        self.sprite = sprite
+        self.rect = rect
+
+    def jump(self, ground):
+        if self.y + self.rect.height == ground:
+            self.vel_y = -25
+            self.jumping = True
+
+    def is_falling(self, ground):
+        if self.y + self.rect.height >= ground:
+            self.y = ground - self.rect.height
+            self.vel_y = 0
+            self.jumping = False
+            terminal = 0
+        else:
+            terminal = self.terminal
+        return terminal
